2c396deded16514b74935e7f9a37d371c1eadbcf
Mikhil Sai N <146115758+MIKHILSAI@users.noreply.github.com>
2026-05-28 20:58:24 +0530

    Add ConditionalLearnNav component for learn page

---
+++ frontend/components/conditional-learn-nav.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { usePathname } from 'next/navigation'
+import { LearnNavigationPill } from "@/components/ui/learn-navigation-pill"
+
+export default function ConditionalLearnNav() {
+  const pathname = usePathname()
+  
+  if (pathname !== '/learn') {
+    return null
+  }
+  
+  return (
+    <>
+      <div className="fixed top-6 left-8 z-50">
+        <img src="/shadow.jpg" alt="Logo" className="h-10 w-auto object-contain drop-shadow-lg" />
+      </div>
+      <div className="fixed top-6 left-0 right-0 z-50 flex justify-center pointer-events-none">
+        <LearnNavigationPill />
+      </div>
+    </>
+  )
+}
