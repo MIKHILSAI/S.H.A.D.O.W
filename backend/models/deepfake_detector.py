c8fa81b07a3b752eddf832696d9f7efc0dab7000
Mikhil Sai N <146115758+MIKHILSAI@users.noreply.github.com>
2026-05-28 21:55:24 +0530

    Create deepfake_detector.py

---
+++ backend/models/deepfake_detector.py
@@ -0,0 +1,26 @@
+import torch
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+from PIL import Image
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+processor = AutoImageProcessor.from_pretrained(
+    "dima806/deepfake_vs_real_image_detection",
+    use_fast=True
+)
+
+model = AutoModelForImageClassification.from_pretrained(
+    "dima806/deepfake_vs_real_image_detection"
+).to(DEVICE)
+
+model.eval()
+
+def predict_image(image: Image.Image):
+    inputs = processor(images=image, return_tensors="pt").to(DEVICE)
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+        probs = torch.softmax(outputs.logits, dim=1)
+
+    fake_prob = probs[0][1].item()
+    return fake_prob
